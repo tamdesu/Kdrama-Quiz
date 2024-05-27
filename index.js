dd9b9f2a88e90021f22574d78d3a66d44ee5e0cd
tamdesu <myfilesharing150@gmail.com>
2024-05-27 11:04:29 +0000

    server

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const { Client, IntentsBitField } = require('discord.js');
 const eventHandler = require('./handlers/eventHandler.js');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log('server started');
+});
 
 const client = new Client({
   intents: [
