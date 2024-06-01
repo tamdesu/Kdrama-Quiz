93eb6ae478a680a74f64775728cbb6668240941c
tamdesu <myfilesharing150@gmail.com>
2024-06-01 09:05:57 +0000

    ehe

--- events/ready/consoleLog.js
+++ events/ready/consoleLog.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
   console.log(`${client.user.tag} is online.`);
   client.user.setPresence({
       activities: [{ name: 'Kdrama Quizes~ <3' }],
-      status: 'invisible',
+      status: 'idle',
   });
   
 };
